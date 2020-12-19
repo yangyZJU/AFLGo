cd9bca7e4cba9038f6e7e81f3938f379c754fc5a
Marcel Boehme <marcel.boehme@acm.org>
2020-12-19 14:23:20 +0000

    Minor changes to aflgo build script.

--- scripts/build/aflgo-build.sh
+++ scripts/build/aflgo-build.sh
@@ -1,6 +1,15 @@
-# build clang & LLVM
+#!/bin/bash
+set -e # exit on error
+
+# Build clang & LLVM
 LLVM_DEP_PACKAGES="build-essential make cmake ninja-build git subversion python2.7 binutils-gold binutils-dev curl wget"
-sudo apt-get install -y $LLVM_DEP_PACKAGES
+apt-get install -y $LLVM_DEP_PACKAGES
+
+export CXX=g++
+export CC=gcc
+unset CFLAGS
+unset CXXFLAGS
+
 mkdir ~/build; cd ~/build
 mkdir llvm_tools; cd llvm_tools
 wget http://releases.llvm.org/4.0.0/llvm-4.0.0.src.tar.xz
@@ -17,12 +26,14 @@ mv cfe-4.0.0.src ~/build/llvm_tools/llvm-4.0.0.src/tools/clang
 mv compiler-rt-4.0.0.src ~/build/llvm_tools/llvm-4.0.0.src/projects/compiler-rt
 mv libcxx-4.0.0.src ~/build/llvm_tools/llvm-4.0.0.src/projects/libcxx
 mv libcxxabi-4.0.0.src ~/build/llvm_tools/llvm-4.0.0.src/projects/libcxxabi
+
 mkdir -p build-llvm/llvm; cd build-llvm/llvm
 cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" \
       -DLLVM_BINUTILS_INCDIR=/usr/include ~/build/llvm_tools/llvm-4.0.0.src
-ninja; sudo ninja install
+ninja; ninja install
+
 cd ~/build/llvm_tools
 mkdir -p build-llvm/msan; cd build-llvm/msan
 cmake -G "Ninja" \
@@ -31,18 +42,24 @@ cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" \
        ~/build/llvm_tools/llvm-4.0.0.src
-ninja cxx; sudo ninja install-cxx
-# install LLVMgold in bfd-plugins
-sudo mkdir /usr/lib/bfd-plugins
-sudo cp /usr/local/lib/libLTO.so /usr/lib/bfd-plugins
-sudo cp /usr/local/lib/LLVMgold.so /usr/lib/bfd-plugins
+ninja cxx; ninja install-cxx
+
+# Install LLVMgold in bfd-plugins
+mkdir /usr/lib/bfd-plugins
+cp /usr/local/lib/libLTO.so /usr/lib/bfd-plugins
+cp /usr/local/lib/LLVMgold.so /usr/lib/bfd-plugins
+
 # install some packages
 export LC_ALL=C
-sudo apt-get update
-sudo apt install -y python-dev python3 python3-dev python3-pip autoconf automake libtool-bin python-bs4 libclang-4.0-dev
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install networkx pydot pydotplus
+apt-get update
+apt install -y python-dev python3 python3-dev python3-pip autoconf automake libtool-bin python-bs4 libclang-4.0-dev
+python3 -m pip install --upgrade pip
+python3 -m pip install networkx pydot pydotplus
+
+export CXX=clang++
+export CC=clang
 # build AFLGo
-cd $HOME; git clone https://github.com/aflgo/aflgo.git
-cd aflgo; make clean all; cd llvm_mode; make clean all
-export AFLGO=$HOME/aflgo
+cd /afl
+make clean all
+pushd llvm_mode; make clean all; popd
+export AFLGO=/afl
